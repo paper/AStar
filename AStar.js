ec75d3c2d3e7915c9e47da4c229199c476710e36
paper <jue_paper@126.com>
2014-08-29 16:14:01 +0800

    2.0.0
    1. 重新整理代码，可读性，欢迎试读 :D
    2. 一点点优化

--- AStar.js
+++ AStar.js
@@ -1,4 +1,4 @@
-/**==========================================================================
+/*=====================================================================
   A* 算法
   
   Author      paper
@@ -6,228 +6,256 @@
   Site        https://github.com/paper
   
   Reference   http://www.policyalmanac.org/games/aStarTutorial.htm
-=============================================================================*/ 
+========================================================================*/
 
+// 地图容器 和 提示消息
+var box = document.getElementById("box");
+var msg = document.getElementById("msg");
 
-var r = [];
-var boxDOM = []
+// 地图数据
+var map = [];
 
-// 行列，可编辑
+/*
+  可编辑的行列
+  
+  这里和canvas的坐标系相反
+  行 - 对应的是 x 轴
+  列 - 对应的是 y 轴
+  
+  也就是说，以左上角为（0，0）原点，
+  x 轴是垂直向下的，表示有多少行，
+  y 抽是水平向右的，表示有多少列
+*/
 var row = 25; // 行
-var col = 25; // 列
+var col = 30; // 列
 
+// 所有节点
+var nodes = [];
+
+// 每个节点的宽度，高度，节点之间的间隙
 var width = 20;
 var height = 20;
 var space = 1;
 
-var begin_elem = null;
-var end_elem = null;
+// 开始节点，结束节点
+var startNode = null;
+var endNode = null;
 
-var box = document.getElementById("box");
-var msg = document.getElementById("msg");
+// letsgo函数 判断是否继续循环的key
+// 如果为false 就不再循环
+var letsgoKey = true;
 
-function init(){
+// 开启列表, 存入坐标的join值 
+// example: [ "3,4", "10,20" ]
+var openList = [];
+
+// 关闭列表, 存入坐标的join值，同上
+var closeList = [];
+
+// 初始化地图基本数据
+// 主要是确定哪些节点是障碍物，哪些不是
+function initMapData(){
   
-  for(var i = 0; i < row; i++){
-    var r_temp = [];
+  var x = 0,
+      y = 0,
+      p = 0.7, // 不是障碍物的概率
+      result = [],
+      temp = null;
+  
+  for(x = 0; x < row; x++){
+    temp = [];
     
-    for(var j = 0; j < col; j++){
+    for(y = 0; y < col; y++){
       
       // 其实可以设置0.5 ，这样障碍物和空格的概率一样，但是这样会出现比较多的死胡同
-      // 为了看效果，估计设置稍微较大的值
-      if( Math.random() < 0.7 ){
-        // 不是障碍物
-        r_temp.push(0);
-      }else{
-        // 障碍物
-        r_temp.push(1);
-      }
+      // 所以为了看效果，设置稍微较大的值
+      // 0: 可以“行走”的空格, 1: 障碍物
+      Math.random() < p ? temp.push(0) : temp.push(1);
     }
     
-    r.push(r_temp);
+    result.push(temp);
   }
   
-}//end init
-
+  return result;
+  
+}//end initMapData
 
 function createMap(){
 
   box.style.display = "none";
   
-  for(var i = 0, len = r.length; i < len; i++){
-    var t = r[i];
-    var dom_temp = [];
-
-    for( var j = 0, len2 = t.length; j<len2; j++ ){
-      var span = document.createElement("span");
+  var x = 0,
+      y = 0,
+      node = null,
       
-      if(t[j] == 1 ){
-        span.className = "block";
+      rowData = null,
+      nodesTemp = null;
+  
+  for(x = 0; x < row; x++){
+    rowData = map[x];
+    nodesTemp = [];
+
+    for(y = 0; y < col; y++ ){
+      node = document.createElement("span");
+      
+      if(rowData[y] == 1 ){
+        node.className = "block";
       }
       
-      span.style.left = (width + space) * j + "px";
-      span.style.top = (height + space) * i + "px";
+      node.style.left = (width  + space) * y + "px";
+      node.style.top  = (height + space) * x + "px";
       
-      span.x = j;
-      span.y = i;
-      span.G = 0;
-      span.H = 0;
-      span.F = span.G + span.H;
+      // 节点初始化
+      node.x = x;
+      node.y = y;
+      node.G = 0;
+      node.H = 0;
+      node.F = 0;
       
-      box.appendChild(span);
-      dom_temp.push(span);
+      box.appendChild(node);
+      nodesTemp.push(node);
     }
     
-    boxDOM.push(dom_temp);
+    nodes.push(nodesTemp);
   }
   
   box.style.display = "block";
   
 }//end createMap
 
-
+// 创建随机的 开始 和 结束 节点
+// A: 开始节点， B: 结束节点
 function createAB(){
-  var A = true;
-  var B = true;
+  var A = true,
+      B = true;
   
   while(A){
-    var x = parseInt( Math.random() * col, 10);
-    var y = parseInt( Math.random() * row, 10);
+    var x = parseInt( Math.random() * row, 10),
+        y = parseInt( Math.random() * col, 10);
     
-    if( r[y][x] == 0 ){
-      boxDOM[y][x].className = "A";
-      boxDOM[y][x].appendChild( document.createTextNode("起") );
+    if( map[x][y] == 0 ){
       A = false;
       
-      begin_elem = boxDOM[y][x];
+      nodes[x][y].className = "A";
+      // 文字改成在css里面定义了
+      // nodes[x][y].innerHTML = "起";
+      startNode = nodes[x][y];
     }
     
   }
   
   while(B){
-    var x = parseInt( Math.random() * col, 10);
-    var y = parseInt( Math.random() * row, 10);
+    var x = parseInt( Math.random() * row, 10),
+        y = parseInt( Math.random() * col, 10);
     
-    if( r[y][x] == 0 && boxDOM[y][x].className != "A" ){
-      boxDOM[y][x].className = "B";
-      boxDOM[y][x].appendChild( document.createTextNode("终") );
+    if( map[x][y] == 0 && nodes[x][y].className != "A" ){
       B = false;
       
-      end_elem = boxDOM[y][x];
+      nodes[x][y].className = "B";
+      //nodes[x][y].innerHTML = "终";
+      endNode = nodes[x][y];
     }
     
   }
   
 }//end createAB
 
-
-init();
-createMap();
-createAB();
-
-// 开启列表, 存入坐标的join值
-// 初始化起始格在开启列表
-var open_list = [ [begin_elem.x, begin_elem.y].join() ];
-
-// 关闭列表, 存入坐标的join值
-var close_list = [];
-
-var father_list = [];
-
-function getElemFormList(v){
-  var r = v.split(",");
-  var x = r[0];
-  var y = r[1];
+// 把节点 node 转化成字符串 item 放入 list
+function pushNode(list, node){
+  var item = [node.x, node.y].join();
   
-  return boxDOM[y][x];
+  list.push( item );
 }
 
-function pushElemIntoList(list, elem){
-  list.push( [elem.x, elem.y].join() );
+// 根据 list 里面的某个元素 item ，得到对应的 节点 
+function getNode(item){
+  var r = item.split(","),
+      x = r[0],
+      y = r[1];
+  
+  return nodes[x][y];
 }
 
-function removeElemFormList(list, elem){
-  var v = [elem.x, elem.y].join();
-  
-  var index = list.indexOf(v);
+// 从 list 里面移除某个 节点 对应的 字符串 item
+function removeNode(list, node){
+  var item = [node.x, node.y].join();
+  var index = list.indexOf(item);
   
   if( index != -1 ){
     list.splice(index, 1);
+    return true;
   }else{
-    return null;
+    return false;
   }
 }
 
-function isInElemFromList(list, elem){
-  var v = [elem.x, elem.y].join();
+// 判断某个节点，是不是 在 某个 list 里面 
+function isIn(list, node){
+  var item = [node.x, node.y].join();
+  var index = list.indexOf(item);
   
-  var index = list.indexOf(v);
-  
-  return index == -1 ? false : true;
+  return index != -1;
 }
 
 // 判断2个节点是不是同一个
-function isSameElem(elemA, elemB){
-  return elemA.x == elemB.x && elemA.y == elemB.y;
+function isSameNode(nodeA, nodeB){
+  return nodeA.x == nodeB.x && nodeA.y == nodeB.y;
 }
 
-// 计算 elem节点，与 end_elem 的 H 估值
-function getH(elem){
-  var x = elem.x;
-  var y = elem.y;
-  
-  var z = Math.abs( end_elem.x - x ) + Math.abs( end_elem.y - y );
-  
-  return z * 10;
+// 计算 某个节点，与 endNode 的 H 估值
+function getH(node){
+  var h = Math.abs( endNode.x - node.x ) + Math.abs( endNode.y - node.y );
+  return h * 10;
 }
 
-// 判断周边的某个点（x，y），和 elem 节点，组成的4节点矩形，另外一条斜线不是障碍物
+// 判断周边的某个点（x，y），和 节点，组成的4节点矩形，另外一条斜线不是障碍物
 // 如果不是障碍物返回true，反之返回false
 function checkPass(x, y, round_x, round_y){
   //左上
   if( round_x < x && round_y < y ){
-    if( r[y-1][x] == 1 && r[y][x-1] == 1 ) return false;
+    if( map[x-1][y] == 1 && map[x][y-1] == 1 ) return false;
   }
   
   //右上
-  if( round_x > x && round_y < y ){
-    if( r[y-1][x] == 1 && r[y][x+1] == 1 ) return false;
+  if( round_x < x && round_y > y ){
+    if( map[x-1][y] == 1 && map[x][y+1] == 1 ) return false;
   }
   
   //左下
-  if( round_x < x && round_y > y ){
-    if( r[y+1][x] == 1 && r[y][x-1] == 1 ) return false;
+  if( round_x > x && round_y < y ){
+    if( map[x+1][y] == 1 && map[x][y-1] == 1 ) return false;
   }
   
   //右下
   if( round_x > x && round_y > y ){
-    if( r[y][x+1] == 1 && r[y+1][x] == 1 ) return false;
+    if( map[x][y+1] == 1 && map[x+1][y] == 1 ) return false;
   }
 
   return true;
 }
 
-// 通过传入一个节点，得到它周边的节点（排除墙）
-// 并设置周边节点对于的G和H，F值
-function getElemRound(elem){
-  var x = elem.x;
-  var y = elem.y;
-  var G = elem.G;
+// 已知一个节点，得到它周边的节点（排除障碍物和closeList等）
+// 并设置周边节点对于的G(如果可以设置)和H，F值
+function getNodeRound(node){
+  var x = node.x,
+      y = node.y,
+      G = node.G;
   
   var round = [
-    [x - 1, y - 1, 14], [x, y - 1, 10], [x + 1, y - 1, 14],
-    [x - 1, y, 10], [x + 1, y, 10],
-    [x - 1, y + 1, 14], [x, y + 1, 10], [x + 1, y + 1, 14]
+    [x - 1, y - 1, 14], [x-1,   y, 10], [x - 1, y + 1, 14],
+    [x,     y - 1, 10],                 [x,     y + 1, 10],
+    [x + 1, y - 1, 14], [x + 1, y, 10], [x + 1, y + 1, 14]
   ];
   
   var roundOk = round.filter(function(v){
     var round_x = v[0];
     var round_y = v[1];
 
-    return round_x >= 0 && round_x < col &&
-           round_y >= 0 && round_y < row &&
-           r[round_y][round_x] != 1 &&
-           close_list.indexOf( [round_x, round_y].join() ) == -1 &&
+    // && 运算时，复杂的计算放后面，这样有助于效率
+    return round_x >= 0 && round_x < row &&
+           round_y >= 0 && round_y < col &&
+           map[round_x][round_y] != 1 &&
+           closeList.indexOf( [round_x, round_y].join() ) == -1 &&
            checkPass(x, y, round_x, round_y)
   });
   
@@ -236,134 +264,149 @@ function getElemRound(elem){
     var y = v[1];
     var g = v[2];
     
-    var el = boxDOM[y][x];
+    var node = nodes[x][y];
     
-    // 如果 el 不在 open_list 就设置新的G值，
+    // 如果 node 不在 openList 就设置新的G值，
     // 不然就先不要改变以前的G值
-    if( !isInElemFromList(open_list, el) ){
-      el.G = G + g;
+    if( !isIn(openList, node) ){
+      node.G = G + g;
     }
-    el.H = getH(el);
-    el.F = el.G + el.H;
     
-    return el;
+    // 每个节点的H值是不变的，如果没有设置过H值，就设置它
+    if( node.H == 0 ){
+      node.H = getH(node);
+    }
+    
+    node.F = node.G + node.H;
+    
+    return node;
   });
   
   return result;
   
-}//end getElemRound
+}//end getNodeRound
 
-// 根据 curElem的G值 和 aimElem的位置，得到新的 G值是多少
-function getElemNewG(curElem, aimElem){
-  var cur_G = curElem.G;
-  var cur_x = curElem.x;
-  var cur_y = curElem.y;
+// 根据当前节点curNode的G值和目标节点aimNode的位置
+// 得到aimNode节点新的G值是多少
+function getNodeNewG(curNode, aimNode){
+  var cur_x = curNode.x,
+      cur_y = curNode.y,
+      cur_G = curNode.G,
+      
+      aim_x = aimNode.x,
+      aim_y = aimNode.y;
   
-  var aim_x = aimElem.x;
-  var aim_y = aimElem.y;
-  
-  if( ( aim_x == cur_x - 1 &&  aim_y == cur_y - 1 )
-     || ( aim_x == cur_x + 1 &&  aim_y == cur_y - 1 )
-     || ( aim_x == cur_x - 1 &&  aim_y == cur_y + 1 )
-     || ( aim_x == cur_x + 1 &&  aim_y == cur_y + 1 ) ){
+  if( ( aim_x == cur_x - 1 &&  aim_y == cur_y - 1 )       //左上
+     || ( aim_x == cur_x - 1 &&  aim_y == cur_y + 1 )     //右上
+     || ( aim_x == cur_x + 1 &&  aim_y == cur_y - 1 )     //左下
+     || ( aim_x == cur_x + 1 &&  aim_y == cur_y + 1 ) ){  //右下
     return cur_G + 14;
   }else{
-    return cur_G + 10;
+    return cur_G + 10; // 上下左右
   }
   
-}
-
-//console.log( getElemRound( begin_elem ) );
+}//end getNodeNewG
 
 // 寻到开启列表中F值最低的格子
-function findLowerF(){
+function findLowestF(){
   
-  if( open_list.length == 1 ){
-    return getElemFormList(open_list[0]);
+  if( openList.length == 1 ){
+    return getNode(openList[0]);
   }
   
   //从 小到大 排序
-  var result = open_list.sort(function(a, b){
-    return getElemFormList(a).F - getElemFormList(b).F;
+  var result = openList.sort(function(a, b){
+    return getNode(a).F - getNode(b).F;
   });
   
-  return getElemFormList( result[0] );
+  return getNode( result[0] );
 }
 
-var letsgoKey = true;
+// let's go :D
 function letsgo( callback ){
   
-  if( !letsgoKey || isInElemFromList( close_list, end_elem ) ){
+  if( !letsgoKey || isIn( closeList, endNode ) ){
     callback && callback(true);
     return;
   }
   
   // 没有解
-  if( open_list.length == 0 ){
+  if( openList.length == 0 ){
     callback && callback(false);
     return;
   }
   
-  var curElem = findLowerF();
+  var curNode = findLowestF();
   
-  console.log( curElem )
+  removeNode(openList, curNode);
+  pushNode(closeList, curNode);
   
-  removeElemFormList(open_list, curElem);
-  pushElemIntoList(close_list, curElem);
-  
-  //  得到周边的值
-  var curElemRound = getElemRound(curElem);
+  // 得到当前节点的周边的可用节点
+  var curNodeRound = getNodeRound(curNode);
   
   // 如果周边没有可走的路
-  // 则循环，这取出 open_list 更合适的值
-  if( curElemRound.length != 0 ){
-  
-    //console.log( curElemRound )
-    //console.log( close_list )
-    //console.log( open_list )
+  // 则循环取出 openList 更合适的值
+  if( curNodeRound.length != 0 ){
     
-    for(var i=0; i<curElemRound.length; i++){
-      var round = curElemRound[i];
+    for(var i=0, len = curNodeRound.length; i < len; i++){
+      var round = curNodeRound[i];
       
-      if( isSameElem(round, end_elem) ){
-        end_elem.father = curElem;
+      if( isSameNode(round, endNode) ){
+        endNode.father = curNode;
         letsgoKey = false;
         break;
       }
       
       //round 不在开启列表
-      if( !isInElemFromList(open_list, round) ){
-        
-        pushElemIntoList(open_list, round);
-        round.father = curElem;
-      }
-      // round 在开启列表
-      else{
+      if( !isIn(openList, round) ){
+        pushNode(openList, round);
+        round.father = curNode;
+      }else{
         // 检查新的G值是否更低
         var old_G = round.G;
-        var new_G = getElemNewG(curElem, round);
+        var new_G = getNodeNewG(curNode, round);
         
         if( new_G < old_G ){
-          round.father = curElem;
+          round.father = curNode;
           round.G = new_G;
         }
       }
       
     }//end for
   }
-
+  
+  // 延迟循环，防止浏览器假死
   setTimeout(function(){
     letsgo(callback);
   }, 10);
   
-  
 }//end letsgo
 
-letsgo(function(key){
-  if( key ){
-    msg.innerHTML = "恭喜你，有解！";
+function showOKMsg(){
+  msg.innerHTML = "恭喜你 , 找到路了！";
+  msg.className = "ok";
+}
+
+function showErrorMsg(){
+  msg.innerHTML = "我擦！走不过去 :(  请刷新页面再试";
+  msg.className = "error";
+}
+
+/*------------------------ Run ------------------------*/
+
+map = initMapData();
+createMap();
+createAB();
+
+// 先初始化开始节点
+pushNode(openList, startNode);
+
+letsgo(function(isOk){
+
+  if( isOk ){
+    showOKMsg();
    
-    var father = end_elem.father;
+    var father = endNode.father;
 
     while( father ){
       if( father.className == "A" ){ break; }
@@ -371,14 +414,9 @@ letsgo(function(key){
       father = father.father;
     }
   }else{
-    msg.innerHTML = "啊哦～无解，请刷新页面再试";
+    showErrorMsg();
   }
   
 });
-
-
-
-
-
 
 
--- README.md
+++ README.md
@@ -1,14 +1,22 @@
 AStar
 =====
 
-AStar 算法
+A* 算法
+## 说明
+
+算法主要来自：http://www.policyalmanac.org/games/aStarTutorial.htm
+而且被翻译成了各种语言，感谢译者们！
 
 
 ## Changelog
 
+##### 2.0.0
+1. 重新整理代码，可读性，欢迎试读 :D
+2. 一点点优化
+
 ##### 1.1.0
-修改一点代码，使得路径不能 "横跨" 2个障碍物。
+修改一点代码，使得路径不能 "横跨" 2个交叉障碍物。
 
 ##### 1.0.0
 初步版本，代码有点乱，目前主要是要实现出来！
-Demo 地址：[Demo](http://paper.github.io/AStar/AStar.html)
+Demo 地址：[Here](http://paper.github.io/AStar/AStar.html)
